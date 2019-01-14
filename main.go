12488347d73b9e5cd00890bc074f561a921c4bba
Chase Hutchins <syntaqx@gmail.com>
2019-01-14 15:33:46 -0700

    adding a comment

--- main.go
+++ main.go
@@ -58,7 +58,8 @@ func main() {
 	}
 }
 
-// Logger ...
+// Logger is a middleware that logs each request, along with some useful data
+// about what was requested, and what the response was.
 func Logger(log *log.Logger) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
