852fac8e94b887f9199ff287a231c8ef329c8516
Chase Hutchins <syntaqx@gmail.com>
2019-01-14 21:44:11 -0700

    cleaning up doc line

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -1,5 +1,5 @@
-// Package main implements the runtime for the serve binary.
-// `serve` is a static http server anywhere you need one.
+// Package main implements the runtime for the serve binary. serve is a static
+// http server anywhere you need one.
 package main
 
 import (
