a1a9070c9977a527a440a2b801eec3ea7d83b793
Chase Pierce <syntaqx@gmail.com>
2019-03-27 00:51:28 -0600

    address staticcheck lint error that was supressed

--- internal/commands/version.go
+++ internal/commands/version.go
@@ -9,6 +9,6 @@ import (
 // Version implements the command `version` which outputs the current binary
 // release version, if any.
 func Version(version string, w io.Writer) error {
-	fmt.Fprintf(w, fmt.Sprintf("serve version %s %s/%s\n", version, runtime.GOOS, runtime.GOARCH))
+	fmt.Fprintf(w, "serve version %s %s/%s\n", version, runtime.GOOS, runtime.GOARCH)
 	return nil
 }
